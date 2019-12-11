387e8c1c05e9304c2276b052a41d46a24adae722
aavshr <aavash09@gmail.com>
2019-12-11 18:55:53 +0100

    clean up start script

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,12 +3,8 @@
 /bin/sh -c exec "nginx"
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start my_first_process: $status"
+  echo "Failed to start nginx: $status"
   exit $status
 fi
 
 ./server 
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# The container exits with an error if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
